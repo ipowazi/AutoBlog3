42343c5b590391af6a53a74e67c6405bb3ddf8e6
Matthew Powers <matthewkevinpowers@gmail.com>
2012-09-22 18:24:30 -0400

    first commit - added models and migrated db

---
+++ config/environment.rb
@@ -0,0 +1,5 @@
+# Load the rails application
+require File.expand_path('../application', __FILE__)
+
+# Initialize the rails application
+AutoBlog3::Application.initialize!
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AutoBlog3::Application.config.secret_token = 'ea0f904250ded2441478548f5fe035f980cc56ac901012eb9ed1507f51bdbdf2b442a4ad12b17a13bd3b9c100d7726acf503802b87f4acfbe35304d8a691687d'
